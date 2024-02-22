cd4adb10fcc192be899a4857aae9f67787da0f27
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2024-02-22 23:23:48 +0200

    add openapi documentation by swagger ui

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import * as fs from 'fs';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import HttpExceptionFilter from './middlewares/http-exception-filter.middleware';
 import userRouter from './routes/user.route';
 import tokenRouter from './routes/token.route';
@@ -13,6 +15,19 @@ const app = express();
 app.use(express.json());
 // Enable CORS for all origins
 app.use(cors());
+
+// swagger api documentation
+const existAPISwaggerJson = fs.existsSync('./api-swagger.json')
+if (existAPISwaggerJson) {
+  const rawData = fs.readFileSync('./api-swagger.json', 'utf-8');
+
+  const jsonData = JSON.parse(rawData.replace(/\$\{[A-Z_]+\}/, (match: string) => {
+    const env = match.replace(/\${([^}]+)}/, '$1');
+    return process.env[env] || '';
+  }));
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(jsonData));
+}
 
 // routes
 app.get('/health', (req, res) => {
